2a9d92be70b1b429cca95acfe55813f2d5f75478
Will Sams <william.sams@wematchwell.com>
2023-03-10 14:38:20 -0500

    Test image automation

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3002;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Example-2 Application - Hello, world!\n');
+    res.end('Example-2 Application - Hello, world! - test ECR\n');
 });
 
 server.listen(port, hostname, () => {
